e1eee972f5117aae22edb4e970db94def1602714
Brandon Giron <162463035+bgironx15@users.noreply.github.com>
2025-01-30 11:13:29 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,41 +1,118 @@
-variable "aws_region" {
-  description = "AWS region to deploy the resources in"
-  type        = string
-  default     = "us-east-1"  # Replace with your desired AWS region
+# Provider configuration
+provider "aws" {
+  region = var.aws_region
+  profile = "saml"
 }
 
-variable "aws_instance_type" {
-  description = "Access Server EC2 instance type"
-  type        = string
-  default     = "t3.small"  # Replace with your desired AWS instance type
+# Create a security group to allow SSH access
+resource "aws_security_group" "AccessServerSecurityGroup" {
+  name        = "AccessServerSecurityGroup"
+  description = "Enable needed access to Access Server"
+  vpc_id      = var.vpc_id
+  
+  ingress {
+    from_port   = 943
+    to_port     = 943
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 945
+    to_port     = 945
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 1194
+    to_port     = 1194
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-variable "vpc_id" {
-  description = "ID of your existing Virtual Private Cloud (VPC)"
-  type        = string
-  default     = "vpc-d08f5daa"  # Replace with your actual VPC ID
+# Fetch the AMI for the desired image
+data "aws_ami" "ubuntu24_image" {
+  most_recent = true
+  owners      = ["099720109477"]
+  
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server*"]  # Replace with the actual Marketplace image name
+  }
 }
 
-variable "subnet_id" {
-  description = "ID of your existing Subnet"
-  type        = string
-  default     = "subnet-c4ac21ea"  # Replace with your actual Subnet ID
+# Create the EC2 instance with Ubuntu 24.04 LTS AMD64
+resource "aws_instance" "OpenVPNAccessServer_Terraform" {
+  ami               = data.aws_ami.ubuntu24_image.id
+  instance_type     = var.aws_instance_type
+  subnet_id         = var.subnet_id
+  key_name          = var.key_name
+  security_groups   = [aws_security_group.AccessServerSecurityGroup.id]
+
+  # Metadata options
+  metadata_options {
+    http_tokens = "optional"   # Allow IMDSv1 and IMDSv2
+    http_endpoint = "enabled"
+  }
+  
+  user_data = <<-EOF
+    #!/bin/bash
+    bash <(curl -fsS https://packages.openvpn.net/as/install.sh | sed 's/PLIST\\x3d\"openvpn-as\"/PLIST\\x3d\"openvpn-as\\x3d2.14.2*\"/') --yes
+    ovpn-init --ec2 --batch --force
+    while [ ! -S /usr/local/openvpn_as/etc/sock/sagent ]; do
+    sleep 1
+    done
+    admin_pw=${var.admin_password}
+    admin_user=${var.admin_username}
+    /usr/local/openvpn_as/scripts/sacli -k 'vpn.server.daemon.ovpndco' -v 'true' ConfigPut
+    /usr/local/openvpn_as/scripts/sacli start
+    exit 0
+  EOF
+
+  
+  tags = {
+    Name = "OpenVPNAccessServer_Terraform"
+  }
 }
 
-variable "key_name" {
-  description = "Name of an existing EC2 KeyPair to enable SSH access to the instance"
-  type        = string
-  default     = "OpenVPNAS-SSH"  # Replace with your actual SSH Key uploaded to AWS Console
+# Outputs
+output "openvpnas_admin_ui" {
+  description = "OpenVPN Access Server Admin WebGUI URL"
+  value       = "https://${aws_instance.OpenVPNAccessServer_Terraform.public_ip}:943/admin"
 }
 
-variable "admin_username" {
-  description = "The OpenVPN Access Server admin username"
-  type        = string
-  default     = "admin1"  # Replace with your desired OpenVPN admin username
+output "openvpnas_client_ui" {
+  description = "OpenVPN Access Server Client WebGUI URL"
+  value       = "https://${aws_instance.OpenVPNAccessServer_Terraform.public_ip}:943"
 }
 
-variable "admin_password" {
-  description = "The OpenVPN Access Server admin password"
-  type        = string
-  default     = "p@$w0rd"  # Replace with your desired OpenVPN admin password
+output "openvpnas_user" {
+  description = "OpenVPN Access Server Admin User"
+  value = {
+    admin_username = var.admin_username
+    admin_password = var.admin_password
+  }
 }
